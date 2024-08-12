a57d229e1f0f5696eb0e78d81303a411cff484ac
Marco Tortoriello <marcotortoriello0@gmail.com>
2024-08-12 16:36:05 +0200

    aggiunto il ruolo "Amministratore cittadini" e dato la possibilità all'admin di promuovere un utente a tale ruolo. i filtri e la pagina per il nuovo ruolo è ancora da sviluppare

--- src/main/java/it/unisa/beingdigital/service/profilo/PromozioneUtenteService.java
+++ src/main/java/it/unisa/beingdigital/service/profilo/PromozioneUtenteService.java
@@ -1,8 +1,10 @@
 package it.unisa.beingdigital.service.profilo;
 
 import it.unisa.beingdigital.storage.entity.Admin;
+import it.unisa.beingdigital.storage.entity.AmministratoreCittadini;
 import it.unisa.beingdigital.storage.entity.Utente;
 import it.unisa.beingdigital.storage.repository.AdminRepository;
+import it.unisa.beingdigital.storage.repository.AmministratoreCittadiniRepository;
 import it.unisa.beingdigital.storage.repository.UtenteRepository;
 import jakarta.validation.constraints.NotNull;
 import java.util.Optional;
@@ -30,6 +32,9 @@ public class PromozioneUtenteService {
   @Autowired
   private CancellazioneAccountService cancellazioneAccountService;
 
+  @Autowired
+  private AmministratoreCittadiniRepository amministratoreCittadiniRepository;
+
   /**
    * Implementa la funzionalità di promozione di un account utente.
    * Si assume che la corretta formulazione dell'id sia stata controllata prima
@@ -51,6 +56,20 @@ public class PromozioneUtenteService {
     Admin admin = new Admin(utente.getNome(), utente.getCognome(), utente.getEmail(),
         utente.getPassword());
     adminRepository.save(admin);
+    return true;
+  }
+
+  public boolean promozioneAmministratoreCittadini(@NotNull Long id) {
+    Optional<Utente> optional = utenteRepository.findById(id);
+    if (optional.isEmpty()) {
+      return false;
+    }
+
+    Utente utente = optional.get();
+    cancellazioneAccountService.cancellazioneUtente(id);
+    utenteRepository.flush();
+    AmministratoreCittadini amministratoreCittadini = new AmministratoreCittadini(utente.getNome(), utente.getCognome(), utente.getEmail(), utente.getPassword());
+    amministratoreCittadiniRepository.save(amministratoreCittadini);
     return true;
   }
 }
